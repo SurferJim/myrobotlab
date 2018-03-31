da1a6dd85b1df14625888a9f3df8b955d1289929
SurferJim <surferjm@optonline.net>
2018-03-31 19:59:35 -0400

    Create RoboContext.java

---
+++ src/main/java/org/myrobotlab/ai/nlu/RoboContext.java
@@ -0,0 +1,16 @@
+package org.myrobotlab.ai.nlu.beans;
+
+import java.util.HashMap;
+
+
+/****************************************************************************
+ * Associates the common elements amongst contextual objects
+ * 
+ * @author nasonj
+ *
+ */
+public class RoboContext {
+
+	private HashMap contextMap;
+	
+}
